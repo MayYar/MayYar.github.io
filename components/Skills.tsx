b4af3431b6dd01431cc12019db2944cea9ef5bd0
Jim Lu <jyunyan.lu@gmail.com>
2022-09-26 12:50:59 -0400

    correct typo

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -96,7 +96,7 @@ function Skills({}: Props) {
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">Skills</h3>
 
       <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
-        Hover over a skill for currency pr
+        Hover over a skill for current pr
       </h3>
 
       <div className="grid grid-cols-4 gap-5">
