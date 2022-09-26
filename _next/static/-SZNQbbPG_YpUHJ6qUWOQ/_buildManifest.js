4313c07292c908956bb3cfc504fad2576d07639e
mayyar <jyunyan.lu@gmail.com>
2022-09-26 16:51:52 +0000

    Deploying to gh-pages from @ mayyar/mayyar.github.io@b4af3431b6dd01431cc12019db2944cea9ef5bd0 🚀

---
+++ _next/static/-SZNQbbPG_YpUHJ6qUWOQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/556-f37fbfdc21f53ec9.js","static/chunks/pages/index-476ccda1b1818cf7.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
